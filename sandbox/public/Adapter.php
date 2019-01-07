b770ebb4512c8a9919ee79b6dd3d31e1bed75e16
vagrant <vagrant@php-training>
2019-01-06 22:11:18 -0800

    2019-01-07 DB: initial examples

---
+++ sandbox/public/Adapter.php
@@ -0,0 +1,12 @@
+<?php
+namespace OurApp\Db;
+
+use Zend\Db\Adapter\Adapter as ZendAdapter;
+use OurApp\Entity\User as UserEntity;
+use OurApp\Model\User as UserModel;
+
+class Adapter extends ZendAdapter
+{
+	// some logic
+}
+
